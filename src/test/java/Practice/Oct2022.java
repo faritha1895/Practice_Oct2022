b758b6c148470a3aefb7fc0eba9ad24a52eff299
Faritha Banu M <Faritha.Banu@ENCORA.COM>
2022-10-12 15:21:41 +0530

    testing

---
+++ src/test/java/Practice/Oct2022.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Oct2022 {
+    public void employeeId(){
+        System.out.println("ID");
+    }
+    public void employeeName(){
+        System.out.println("Name");
+    }
+    public static void main(String arg[]){
+        Oct2022 o=new Oct2022();
+        o.employeeId();
+        o.employeeName();
+    }
+}
