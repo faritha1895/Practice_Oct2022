dad2ddaebb10e2c713b6877a9878350b5af4313c
Faritha Banu M <Faritha.Banu@ENCORA.COM>
2022-10-12 16:14:02 +0530

    updating

--- src/test/java/Practice/Oct2022.java
+++ src/test/java/Practice/Oct2022.java
@@ -7,9 +7,13 @@ public class Oct2022 {
     public void employeeName(){
         System.out.println("Name");
     }
+    public void employeeLocation(){
+        System.out.println("Location");
+    }
     public static void main(String arg[]){
         Oct2022 o=new Oct2022();
         o.employeeId();
         o.employeeName();
+        o.employeeLocation();
     }
 }
